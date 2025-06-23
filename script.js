e57c7bad4dd2a31be1d92585a9ef98228198e7b3
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-23 20:43:14 +0700

    update

---
+++ script.js
@@ -0,0 +1,35 @@
+// refactor
+//id d1 - d4 and s1- s4
+const d = [];
+const s = [];
+
+const plus = [];
+const minus = [];
+
+// icon plus and minus
+
+for (let i = 1; i <= 4; i++) {
+  d[i] = document.getElementById(`d${i}`);
+  s[i] = document.getElementById(`s${i}`);
+
+  plus[i] = document.getElementById(`plus${i}`);
+  minus[i] = document.getElementById(`minus${i}`);
+
+  d[i].addEventListener("click", function () {
+  const isOpen = s[i].classList.contains("show");
+
+  // Tutup semua dulu
+  for (let j = 1; j <= 4; j++) {
+    s[j].classList.remove("show");
+    plus[j].classList.remove("hide");
+    minus[j].classList.add("hide");
+  }
+
+  // Kalau tadi belum terbuka, buka yang diklik
+  if (!isOpen) {
+    s[i].classList.add("show");
+    plus[i].classList.add("hide");
+    minus[i].classList.add("visible");
+  }
+});
+}
